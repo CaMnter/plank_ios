38a647daad5e51beda582bfd44260bed0dba4ec9
jiangecho <jiangyingji1@163.com>
2015-11-08 16:11:50 +0800

    init commit

---
+++ plank_iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  plank_iOS
+//
+//  Created by jiangecho on 15/11/8.
+//  Copyright © 2015年 jiangecho. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
