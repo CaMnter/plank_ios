b9f5d2f3bddc0743c5558b9867f5316184fb4c7a
jiangecho <jiangyingji1@163.com>
2015-11-13 15:51:29 +0800

    add WebViewController

---
+++ plank_iOS/WebViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WebViewController.swift
+//  plank_iOS
+//
+//  Created by jiangecho on 15/11/13.
+//  Copyright © 2015年 jiangecho. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBAction func finish(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
