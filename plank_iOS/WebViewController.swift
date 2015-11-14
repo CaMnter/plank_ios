2aeaadf2d1c6c968f4be0a67ba2fc58618a02e73
jiangecho <jiangyingji1@163.com>
2015-11-14 21:54:24 +0800

    add controller to storyboard

--- plank_iOS/WebViewController.swift
+++ plank_iOS/WebViewController.swift
@@ -26,11 +26,11 @@ class WebViewController: UIViewController {
         self.title = "webView"
         loadData()
         
-                    //reading
-            do {
-                htmlTemplateCotent = try String(contentsOfFile: NSBundle.mainBundle().pathForResource("post.html", ofType: nil, inDirectory: "html")!, encoding: NSUTF8StringEncoding)
-            }
-            catch {/* error handling here */}
+        //reading
+        do {
+            htmlTemplateCotent = try String(contentsOfFile: NSBundle.mainBundle().pathForResource("post.html", ofType: nil, inDirectory: "html")!, encoding: NSUTF8StringEncoding)
+        }
+        catch {/* error handling here */}
     }
     
     override func didReceiveMemoryWarning() {
@@ -41,10 +41,10 @@ class WebViewController: UIViewController {
     func loadData(){
         Alamofire.request(.GET, "http://plankmp.sinaapp.com/?json=get_post&post_id=13", parameters: nil)
             .responseJSON { response in
-//                print(response.request)  // original URL request
-//                print(response.response) // URL response
-//                print(response.data)     // server data
-//                print(response.result)   // result of response serialization
+                //                print(response.request)  // original URL request
+                //                print(response.response) // URL response
+                //                print(response.data)     // server data
+                //                print(response.result)   // result of response serialization
                 
                 //response.result.value!["content"]
                 if let json = response.result.value {
@@ -52,14 +52,14 @@ class WebViewController: UIViewController {
                         let content:String = post!["content"] as! String
                         let title:String  = post!["title"] as! String
                         self.title = title
-                       let tmp = self.htmlTemplateCotent.stringByReplacingOccurrencesOfString("content", withString: content)
+                        let tmp = self.htmlTemplateCotent.stringByReplacingOccurrencesOfString("content", withString: content)
                         self.webView.loadHTMLString(tmp, baseURL: nil)
                     }
-               }
+                }
                 
-//                if let JSON = response.result.value {
-//                    print("JSON: \(JSON)")
-//                }
+                //                if let JSON = response.result.value {
+                //                    print("JSON: \(JSON)")
+                //                }
         }
     }
     
