2ff191b7fb0959d7af44de0a2e058f81cd9478fe
jiangecho <jiangyingji1@163.com>
2015-11-16 16:30:24 +0800

    fix bug: back item color

--- plank_iOS/Coding_iOS/Controllers/RootControllers/Tweet_RootViewController.m
+++ plank_iOS/Coding_iOS/Controllers/RootControllers/Tweet_RootViewController.m
@@ -105,6 +105,8 @@
 //    
     //UIBarButtonItem *leftBarItem =[UIBarButtonItem itemWithIcon:@"search_Nav" showBadge:NO target:self action:@selector(searchItemClicked:)];
     UIBarButtonItem *leftBarItem =[UIBarButtonItem itemWithBtnTitle:@"close" target:self action:@selector(myDismiss)];
+    leftBarItem.tintColor = [UIColor blackColor];
+
     [self.parentViewController.navigationItem setLeftBarButtonItem:leftBarItem animated:NO];
     
     _tweetsDict = [[NSMutableDictionary alloc] initWithCapacity:4];
