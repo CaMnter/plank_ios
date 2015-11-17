60825407f2924b40b70d13511709fa095499ebc6
jiangecho <jiangyingji1@163.com>
2015-11-17 16:28:30 +0800

    add WebViewController

--- plank_iOS/DiscoverController.swift
+++ plank_iOS/DiscoverController.swift
@@ -20,9 +20,11 @@ class DiscoverController: UITableViewController {
         // TODO
         switch segue.identifier! {
         case "showFitnessPlus":
+            let webViewController = segue.destinationViewController as! WebViewController
+            webViewController.url = "http://mp.diaoba.wang/api/wx/daily_post_list?category_id=1"
             break
         case "showContributor":
-            let postWebViewController = segue.destinationViewController as! PostWebViewController;
+            let postWebViewController = segue.destinationViewController as! PostWebViewController
             postWebViewController.postID = 207;
             break
         case "showTweet":
---
+++ plank_iOS/WebViewController.swift
@@ -0,0 +1,36 @@
+//
+//  WebViewController.swift
+//  plank_iOS
+//
+//  Created by jiangecho on 15/11/13.
+//  Copyright © 2015年 jiangecho. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class WebViewController: UIViewController {
+    
+    var url = "http://mp.diaoba.wang/api/wx/daily_post_list?category_id=1"
+    let htmlTemplateFile = "post.html"
+    var htmlTemplateCotent = ""
+    
+    @IBOutlet weak var webView: UIWebView!
+    @IBAction func finish(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: url)!))
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
