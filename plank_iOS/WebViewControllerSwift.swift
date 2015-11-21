a403fc95e180b3bb79274a2f4635c74d27367d8f
jiangecho <jiangyingji1@163.com>
2015-11-21 22:21:17 +0800

    add progressBar to webview

--- plank_iOS/WebViewControllerSwift.swift
+++ plank_iOS/WebViewControllerSwift.swift
@@ -11,6 +11,10 @@ import Alamofire
 
 class WebViewControllerSwift: UIViewController, UIWebViewDelegate {
     
+    @IBOutlet weak var progressView: UIProgressView!
+    var theBool: Bool?
+    var myTimer: NSTimer?
+    
     var url = ""
     let htmlTemplateFile = "post.html"
     var htmlTemplateCotent = ""
@@ -22,8 +26,12 @@ class WebViewControllerSwift: UIViewController, UIWebViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.theBool = false
+        self.progressView.progress = 0
+        self.myTimer = NSTimer.scheduledTimerWithTimeInterval(0.1667, target: self, selector: "timerCallback", userInfo: nil, repeats: true)
         // Do any additional setup after loading the view, typically from a nib.
         webView.loadRequest(NSURLRequest(URL: NSURL(string: url)!))
+
 
     }
     
@@ -44,7 +52,23 @@ class WebViewControllerSwift: UIViewController, UIWebViewDelegate {
     func webViewDidFinishLoad(webView: UIWebView) {
         let title = webView.stringByEvaluatingJavaScriptFromString("document.title")
         self.title = title
+        self.theBool = true
     }
     
+    func timerCallback() {
+        if self.theBool! {
+            if self.progressView.progress >= 1 {
+                self.progressView.hidden = true
+                self.myTimer!.invalidate()
+            } else {
+                self.progressView.progress += 0.1
+            }
+        } else {
+            self.progressView.progress += 0.05
+            if self.progressView.progress >= 0.95 {
+                self.progressView.progress = 0.95
+            }
+        }
+    }
 
 }
