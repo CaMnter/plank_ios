2de651b953baf91323d4dd0b4020dcfdde6abd3a
jiangecho <jiangyingji1@163.com>
2015-11-28 22:52:34 +0800

    fix bug: gif image lead to crash issue

--- plank_iOS/Coding_iOS/Vendor/SDWebImage/UIImage+GIF.h
+++ plank_iOS/Coding_iOS/Vendor/SDWebImage/UIImage+GIF.h
@@ -7,12 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "YLGIFImage.h"
 
 @interface UIImage (GIF)
 
 + (UIImage *)sd_animatedGIFNamed:(NSString *)name;
 
 + (UIImage *)sd_animatedGIFWithData:(NSData *)data;
+
+- (UIImage *)sd_animatedImageByScalingAndCroppingToSize:(CGSize)size;
 
 @end
